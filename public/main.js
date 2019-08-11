8ee98eef9c43b7909a701bafaeab1eebeedbe04e
Ganesh <ganeshh123@users.noreply.github.com>
2019-08-11 01:23:33 +0100

    Core App Ready
    Need to Style

---
+++ public/main.js
@@ -0,0 +1,41 @@
+const searchElement = document.querySelector('[data-city-search]')
+const searchBox = new google.maps.places.SearchBox(searchElement)
+searchBox.addListener('places_changed', () => {
+    const place = searchBox.getPlaces()[0]
+    if (place == null) return
+    const latitude = place.geometry.location.lat()
+    const longitude = place.geometry.location.lng()
+    fetch('/weather', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+            latitude: latitude,
+            longitude: longitude
+        })
+    }).then(res => res.json()).then(data => {
+        console.log(data)
+        setWeatherData(data, place.formatted_address)
+    })
+})
+
+const icon = new Skycons({ color: '#222' })
+const locationElement = document.querySelector('[data-location')
+const statusElement = document.querySelector('[data-status')
+const tempElement = document.querySelector('[data-temp')
+const windElement = document.querySelector('[data-wind')
+const precElement = document.querySelector('[data-prec')
+icon.set('icon', 'partly-cloudy-day')
+
+function setWeatherData(data, place) {
+    locationElement.textContent = place
+    statusElement.textContent = data.summary
+    tempElement.textContent = data.summary
+    windElement.textContent = data.windSpeed
+    precElement.textContent = data.precipProbability * 100 + '%'
+    icon.set('icon', data.icon)
+    icon.play()
+
+}
