d1cea0ea95bf57c081f8bdb8324d66730da19d04
Ganesh <ganeshh123@users.noreply.github.com>
2019-08-11 11:28:14 +0100

    Update server.js

--- server.js
+++ server.js
@@ -18,6 +18,6 @@ app.post('/weather', (req, res) => {
     }).then(data => res.json(data.data.currently))
 })
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log('Server Started')
 })
