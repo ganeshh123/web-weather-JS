8bed049da5dc658c990aab01ee112cfbf7b2cc12
Ganesh <ganeshh123@users.noreply.github.com>
2019-09-19 13:59:17 +0100

    Fix for outdated date/time showing when changing location

--- public/main.js
+++ public/main.js
@@ -1,6 +1,7 @@
 const searchElement = document.querySelector('[data-city-search]')
 const searchBox = new google.maps.places.SearchBox(searchElement)
 var timeData = {};
+
 searchBox.addListener('places_changed', () => {
     const place = searchBox.getPlaces()[0]
     if (place == null) return
@@ -19,6 +20,7 @@ searchBox.addListener('places_changed', () => {
     }).then(res => res.json()).then(data => {
         timeData = data;
         console.log(timeData)
+        setTimeData(timeData)
     })
     fetch('/weather', {
         method: 'POST',
@@ -50,13 +52,7 @@ icon.set('icon', 'partly-cloudy-day')
 function setWeatherData(data, place) {
 
     currently = data.currently
-    timeString = timeData.formatted
-    console.log(timeString)
-    console.log(timeString.length)
-    time = timeData.timestamp - 3600;
 
-    timeElement.style.display = "inline-block";
-    dateElement.style.display = "inline-block";
     localInfoElement.style.backgroundColor = "rgba(0,0,0,0.1)";
     //localInfoElement.style.margin = "0px 0px 10px 0px";
 
@@ -64,8 +60,7 @@ function setWeatherData(data, place) {
     locationElement.textContent = place.formatted_address;
     //timeElement.textContent = timeConvert(time) + " |";
     //dateElement.textContent = dateConvert(time);
-    timeElement.textContent = timeString.slice(11, 16) + " |"
-    dateElement.textContent = timeString.slice(0, 10)
+
     statusElement.textContent = currently.summary;
     tempElement.textContent = currently.temperature + " ℃ | " + Math.round(convertTemp(currently.temperature) * 100) / 100 + " ℉";
     windElement.textContent = currently.windSpeed + " kph | " + Math.round(convertSpeed(currently.windSpeed) * 100) / 100 + " mph"
@@ -102,39 +97,52 @@ function setWeatherData(data, place) {
         case "partly-cloudy-night":
             document.body.style.background = "url('img/night.jpg')";
             break;
-
     }
     icon.play()
+}
 
-    function convertTemp(input) {
-        return (input * 9 / 5) + 32;
-    }
+function setTimeData(timeData) {
+    timeString = timeData.formatted
+    console.log(timeString)
+    console.log(timeString.length)
+    time = timeData.timestamp - 3600;
+    timeElement.style.display = "inline-block";
+    dateElement.style.display = "inline-block";
+    localInfoElement.style.backgroundColor = "rgba(0,0,0,0.1)";
+    timeElement.textContent = timeString.slice(11, 16) + " |"
+    dateElement.textContent = timeString.slice(0, 10)
 
-    function convertSpeed(input) {
-        return input / 1.609344;
-    }
+}
 
-    function timeConvert(t) {
-        var dt = new Date(t * 1000);
-        var hr = dt.getHours();
-        var m = "0" + dt.getMinutes();
-        var s = "0" + dt.getSeconds();
-        return hr + ':' + m.substr(-2);
-    }
 
-    function dateConvert(t) {
-        var date = new Date(t * 1000);
 
-        var weekday = new Array(7);
-        weekday[0] = "Sun";
-        weekday[1] = "Mon";
-        weekday[2] = "Tue";
-        weekday[3] = "Wed";
-        weekday[4] = "Thu";
-        weekday[5] = "Fri";
-        weekday[6] = "Sat";
+function convertTemp(input) {
+    return (input * 9 / 5) + 32;
+}
 
-        return weekday[date.getDay()] + " " + date.getDate() + "-" + (date.getMonth() + 1).toString() + "-" + date.getFullYear();
-    }
+function convertSpeed(input) {
+    return input / 1.609344;
+}
 
+function timeConvert(t) {
+    var dt = new Date(t * 1000);
+    var hr = dt.getHours();
+    var m = "0" + dt.getMinutes();
+    var s = "0" + dt.getSeconds();
+    return hr + ':' + m.substr(-2);
+}
+
+function dateConvert(t) {
+    var date = new Date(t * 1000);
+
+    var weekday = new Array(7);
+    weekday[0] = "Sun";
+    weekday[1] = "Mon";
+    weekday[2] = "Tue";
+    weekday[3] = "Wed";
+    weekday[4] = "Thu";
+    weekday[5] = "Fri";
+    weekday[6] = "Sat";
+
+    return weekday[date.getDay()] + " " + date.getDate() + "-" + (date.getMonth() + 1).toString() + "-" + date.getFullYear();
 }
